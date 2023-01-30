dc08642a593918f6fe58e3f86f314979de34ac26
denis.lindner <denis.lindner@redhotmagma.de>
2023-01-30 09:32:21 +0100

    push local project

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import "../styles/App.css";
+import CreateLink from "./CreateLink";
+import LinkList from "./LinkList";
+import Header from "./Header";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<Navigate replace to={"/new/1"} />} />
+          <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/top" element={<LinkList />} />
+          <Route path="new/:page" element={<LinkList />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
